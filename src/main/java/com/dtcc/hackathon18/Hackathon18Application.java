672eec2eee8cf65b2da58f801544b6af6fe73147
Jayaprakash Jayabalan <jayabalj@in.ibm.com>
2018-05-08 13:13:28 +0530

    add

---
+++ src/main/java/com/dtcc/hackathon18/Hackathon18Application.java
@@ -0,0 +1,12 @@
+package com.dtcc.hackathon18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon18Application.class, args);
+	}
+}
